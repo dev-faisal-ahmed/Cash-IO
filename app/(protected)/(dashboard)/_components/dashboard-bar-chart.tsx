351c5636a43428a7260d0c8e4fd39cf9f7dfb099
Os-Traveller <ost.faisal.ahmed@gmail.com>
2023-10-23 12:59:31 +0600

    bar chart implimented

--- app/(protected)/(dashboard)/_components/dashboard-bar-chart.tsx
+++ app/(protected)/(dashboard)/_components/dashboard-bar-chart.tsx
@@ -3,43 +3,19 @@
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 import { indigo } from 'tailwindcss/colors';
 
-const data = [
-  {
-    name: 'Jan',
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: 'Feb',
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: 'Mar',
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: 'Apr',
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: 'May',
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: 'Jun',
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: 'Jul',
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-];
+type DashboardBarChartProps = {
+  transactions: {
+    date: string;
+    amount: number;
+  }[];
+};
 
-export function DashboardBarChart() {
+export function DashboardBarChart({ transactions }: DashboardBarChartProps) {
   return (
     <ResponsiveContainer width='100%' height={350}>
-      <BarChart data={data}>
+      <BarChart data={transactions}>
         <XAxis
-          dataKey='name'
+          dataKey='date'
           stroke='#888888'
           fontSize={12}
           tickLine={false}
@@ -52,7 +28,7 @@ export function DashboardBarChart() {
           axisLine={false}
           tickFormatter={(value) => `$${value}`}
         />
-        <Bar dataKey='total' fill={indigo[700]} radius={[15, 15, 0, 0]} />
+        <Bar dataKey='amount' fill={indigo[700]} radius={[15, 15, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   );
